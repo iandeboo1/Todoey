cee44f66fbcda0ecc027d1d722e28701cc108e5c
Ian DeBoo <ianjdeboo@icloud.com>
2019-04-10 16:55:58 -0600

    Convert to MVC design pattern complete

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray : [String] = []
+    var itemArray = [Item]
     
     let defaults = UserDefaults.standard
-    //this accesses a small storage area allocated to each app that will retain default settings across app launches
+    //this accesses a small storage area allocated to each app that will retain default settings across app launches; storing too much data will cause extremely long app loading times
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+        let newItem = Item()
+        newItem.label = "Eat Poison"
+        itemArray.append(newItem)
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Items] {
             itemArray = items
         }
         
@@ -36,7 +40,12 @@ class TodoListViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         //this line creates the cell item and names it "cell", links to the protype cell with the name "ToDo..."; also make sure that the i in index path is lowercase
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.label
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        //this is called a Ternary Operator and basically takes the places of and shortens an if-else statement in certain cases
         
         return cell
     }
@@ -47,11 +56,9 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
     }
     
 //////////////////////////////////////////////////////////////////////////////////
@@ -65,7 +72,12 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) {
             (action) in
-            self.itemArray.append(textField.text!)
+            
+            let newItem = Item()
+            newItem.label = textField.text!
+            
+            self.itemArray.append(newItem)
+
             self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             //this line sets a key value pair to be saved; the first part is the value and the second part is the key name
             self.tableView.reloadData()
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ian DeBoo on 4/10/19.
+//  Copyright © 2019 Ian DeBoo. All rights reserved.
+//
+
+import UIKit
+
+class Item: UIViewController {
+
+    var label : String = ""
+    var done : Bool = false
+
+}
